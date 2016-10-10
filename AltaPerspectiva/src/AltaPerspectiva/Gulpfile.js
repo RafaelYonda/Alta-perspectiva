0f4a340445635db87dc95f3c8ef98bfa071d3ea6
sayedur-nascenia <sayedur@nascenia.com>
2016-10-10 19:09:41 +0600

    Angular 2 added.

--- AltaPerspectiva/src/AltaPerspectiva/Gulpfile.js
+++ AltaPerspectiva/src/AltaPerspectiva/Gulpfile.js
@@ -1,86 +1,48 @@
-﻿/// <binding Clean='clean' />
-"use strict";
-var gulp = require("gulp"),
-    concat = require("gulp-concat"),
-    filter = require("gulp-filter"),
-    less = require("gulp-less"),
-    rename = require("gulp-rename"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    bower = require("gulp-bower"),
-    del = require("del"),
-    mainBowerFiles = require("main-bower-files");
+﻿var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
 
-var paths = {
-    wwwLib: "./wwwroot/lib/",
-    wwwStyles: "./wwwroot/css/"
-};
+var destPath = './libs/';
 
-//===========lib files like:bower component and others=============
-
-//Clean all from wwwroot/lib
-gulp.task("clean:lib", function () {
-    del([paths.wwwLib]);
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
 });
 
-
-//
-// Copy Bower components from bower directory to Scripts/lib directory
-gulp.task("copy:bower-components", function () {
-
-    var jsFilter = filter(["**/**/*.js"], { restore: true });
-    var cssFilter = filter(["**/*.css"], { restore: true });
-    var fontsFilter = filter(["**/*.woff2", "**/*.eot", "**/*.svg", "**/*.ttf", "**/*.woff"], { restore: true });
-
-    return gulp.src(mainBowerFiles({
-        overrides: {
-            'bootstrap': {
-                main: [
-                    "./dist/js/bootstrap.js",
-                    "./dist/css/*.css",
-                    "./dist/*.css",
-                    "./dist/fonts/*.*"
-                ]
-            },
-            'fontawesome': {
-                main: [
-                    "./css/font-awesome.css",
-                    "./fonts/*.*"
-                ]
-            }
-        }
-    }),
-        { base: "./bower_components" })
-
-        // Copy vendor js files from bower_components, minify and publish in /scripts/lib
-        .pipe(jsFilter)
-        .pipe(gulp.dest(paths.wwwLib))
-        .pipe(jsFilter.restore)
-
-        // Copy vendor css files from bower_components, minify and publish in /Content/lib
-        .pipe(cssFilter)
-        .pipe(gulp.dest(paths.wwwLib))
-        .pipe(cssFilter.restore)
-
-        // Copy vendor font files from bower_components, /Content/fonts
-        .pipe(fontsFilter)
-        .pipe(gulp.dest(paths.wwwLib))
-        .pipe(fontsFilter.restore);
+gulp.task("scriptsNStyles", () => {
+    gulp.src([
+            'core-js/client/**',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/dist/js/bootstrap.*js'
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./libs"));
 });
 
-//=============Custom file=================
+var tsProject = ts.createProject('tsScripts/tsconfig.json', {
+    typescript: require('typescript')
+});
 
-//
-// Delete all custom stylesheets from /www/css directory
-//
-gulp.task("clean:css", function () {
-    del([paths.wwwStyles]);
+gulp.task('ts', function (done) {
+    //var tsResult = tsProject.src()
+    var tsResult = gulp.src([
+            "tsScripts/*.ts"
+    ])
+        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+    return tsResult.js.pipe(gulp.dest('./Scripts'));
 });
-//
-// Compile style sheets 
-//
-gulp.task("copy:css", function () {
-    return gulp.src(["./Styles/site.less"])
-        .pipe(less())
-        .pipe(gulp.dest(paths.wwwStyles));
+
+gulp.task('watch', ['watch.ts']);
+
+gulp.task('watch.ts', ['ts'], function () {
+    return gulp.watch('tsScripts/*.ts', ['ts']);
 });
+
+gulp.task('default', ['scriptsNStyles', 'watch']);
