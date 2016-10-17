3297578a13d446bd43d9a41a94551f3369eaaa5e
sayedur-nascenia <sayedur@nascenia.com>
2016-10-17 17:29:29 +0600

    Routing partially added.

--- AltaPerspectiva/src/AltaPerspectiva/tsScripts/app/core/core.module.ts
+++ AltaPerspectiva/src/AltaPerspectiva/tsScripts/app/core/core.module.ts
@@ -1,7 +1,13 @@
 ﻿import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ApNav } from './nav/apnav';
 
 @NgModule({
+    //imports: [
+    //    RouterModule.forRoot([
+    //        { path: 'hero/:id', component: HeroDetailComponent },
+    //    ])
+    //],
     declarations: [ ApNav],
     exports: [ApNav]
 })
