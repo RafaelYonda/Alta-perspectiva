dccfd547d3461cc0af7c51bc648b61e3ac3efb27
sayedur-nascenia <sayedur@nascenia.com>
2016-10-18 11:52:33 +0600

    partially done.

--- AltaPerspectiva/src/AltaPerspectiva/tsScripts/app/core/core.module.ts
+++ AltaPerspectiva/src/AltaPerspectiva/tsScripts/app/core/core.module.ts
@@ -1,14 +1,16 @@
-﻿import { NgModule } from '@angular/core';
+﻿/// <reference path="proyectos-detail.ts" />
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApNav } from './nav/apnav';
+import { ProyectosDetail } from './proyectos-detail';
 
 @NgModule({
-    //imports: [
-    //    RouterModule.forRoot([
-    //        { path: 'hero/:id', component: HeroDetailComponent },
-    //    ])
-    //],
-    declarations: [ ApNav],
-    exports: [ApNav]
+    imports: [
+        RouterModule.forRoot([
+            { path: 'proyectos', component: ProyectosDetail },
+        ])
+    ],
+    declarations: [ApNav, ProyectosDetail],
+    exports: [ApNav, ProyectosDetail]
 })
 export class CoreModule { }
---
+++ AltaPerspectiva/src/AltaPerspectiva/tsScripts/app/core/proyectos-detail.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    template: '<div>Proyectos detail</div>'
+})
+export class ProyectosDetail { }
