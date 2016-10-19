8778dcb5cf3689e8823022a0a9425f95d6f502c0
sayedur-nascenia <sayedur@nascenia.com>
2016-10-19 18:05:33 +0600

    service added. inprogress.

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/home.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/home.component.ts
@@ -1,6 +1,18 @@
-﻿import { Component } from '@angular/core';
+﻿/// <reference path="category.ts" />
+import { Component } from '@angular/core';
+import { CategoryService } from './services/category.service'
+import { Category } from './category'
 
 @Component({
     templateUrl: 'js/app/core/home.component.html',
+    providers: [CategoryService]
 })
-export class HomeComponent { }
+export class HomeComponent {
+    categories: Category[];
+
+    constructor(private categoryService: CategoryService) { }
+
+    getCategories(): void {
+        this.categories = this.categoryService.getCategories();
+    }
+}
