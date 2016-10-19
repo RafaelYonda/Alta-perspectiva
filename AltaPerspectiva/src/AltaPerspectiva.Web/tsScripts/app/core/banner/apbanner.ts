08d37848ecff6c659ab67fda5fd99ae313cc23df
Sharif Rifat <sharif@nascenia.com>
2016-10-19 10:53:23 +0600

    Emergency commit for core.ts fix

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/banner/apbanner.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/banner/apbanner.ts
@@ -1,6 +1,4 @@
 ﻿import { Component } from '@angular/core';
-import {Image} from './image.interface';
-
 
 @Component({
     selector: 'ap-banner',
@@ -10,11 +8,5 @@ import {Image} from './image.interface';
 })
 export class ApBanner {
     public heroes: number[] = [ 1,2,3,4,5,6,7,8];
-    public images = IMAGES;
 }
-var IMAGES: Image[] = [
-    { "title": "We are covered", "url": "../images/background.png" },
-    { "title": "Generation Gap", "url": "../images/background.png" },
-    { "title": "Potter Me", "url": "../images/background.png" },
-    { "title": "Pre-School Kids", "url": "../images/background.png" },
-];
+
