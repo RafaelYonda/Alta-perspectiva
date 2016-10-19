c79ddbd1f68b431b6aed119641d16a632c9a0e62
sayedur-nascenia <sayedur@nascenia.com>
2016-10-19 13:05:29 +0600

    Routing updated.

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/core.module.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/core.module.ts
@@ -1,8 +1,7 @@
-﻿/// <reference path="../learning/learnhome.ts" />
-/// <reference path="../project/projecthome.ts" />
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApNav } from './nav/apnav';
+import { HomeComponent } from './home.component';
 import { QuestionHome } from '../questions/questionhome';
 import { LearnHome } from '../learning/learnHome';
 import { ProjectHome } from '../project/projectHome';
@@ -10,13 +9,13 @@ import { ProjectHome } from '../project/projectHome';
 @NgModule({
     imports: [
         RouterModule.forRoot([
-            { path: '', redirectTo: '/question', pathMatch: 'full' },
+            { path: '', component: HomeComponent },
             { path: 'question', component: QuestionHome },
             { path: 'learn', component: LearnHome },
             { path: 'project', component: ProjectHome },
         ])
     ],
-    declarations: [ApNav, QuestionHome, LearnHome, ProjectHome,],
-    exports: [ApNav, QuestionHome, LearnHome, ProjectHome, RouterModule]
+    declarations: [ApNav, HomeComponent, QuestionHome, LearnHome, ProjectHome,],
+    exports: [ApNav, HomeComponent, QuestionHome, LearnHome, ProjectHome, RouterModule]
 })
 export class CoreModule { }
---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/home.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'js/app/core/home.component.html',
+})
+export class HomeComponent { }
