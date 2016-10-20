46dba8842d68370422d532b36c5ba8e41ce9271b
Sharif Rifat <sharif@nascenia.com>
2016-10-20 20:35:02 +0600

    Remove Unnecessary JS files

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/tabs/tabs.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/tabs/tabs.component.ts
@@ -6,10 +6,19 @@
 })
 export class TabsComponent {
     catagories: any = [
-        { id: 1, name: 'categoty1', icon: 'icon1', active:true },
-        { id: 2, name: 'categoty2', icon: 'icon2' },
-        { id: 3, name: 'categoty3', icon: 'icon3'  },
-        { id: 4, name: 'categoty4', icon: 'icon4'  },
-        { id: 5, name: 'categoty5', icon: 'icon5' }
+        { id: 1, name: 'categoty1', icon: 'icon1', active:'active' },
+        { id: 2, name: 'categoty2', icon: 'icon2',active:''},
+        { id: 3, name: 'categoty3', icon: 'icon3',active:''},
+        { id: 4, name: 'categoty4', icon: 'icon4',active:''},
+        { id: 5, name: 'categoty5', icon: 'icon5',active:''}
     ];
+    selectTab(id){
+        console.log(id);
+        this.catagories.forEach(element => {
+            if(element.id==id){
+                element.active='active';
+            }
+            else element.active='';
+        });
+    }
 }
