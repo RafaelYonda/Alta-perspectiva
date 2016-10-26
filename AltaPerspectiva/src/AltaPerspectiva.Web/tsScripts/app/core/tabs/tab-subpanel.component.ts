03ecbf4471f6138dbca0e15d1fa864b3aeb07ba9
sayedur-nascenia <sayedur@nascenia.com>
2016-10-26 19:25:55 +0600

    QUestion panel updated.

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/tabs/tab-subpanel.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/tabs/tab-subpanel.component.ts
@@ -1,19 +1,27 @@
-﻿import { Component } from '@angular/core';
+﻿/// <reference path="../../services/question.ts" />
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { QuestionAnswerService } from '../../services/questionAnswer.service';
+import { Question } from '../../services/question';
 
 @Component({
     selector: 'ap-tab-subpanel',
     templateUrl: 'js/app/core/tabs/tab-subpanel.component.html',
+    providers: [QuestionAnswerService]
 })
 export class TabSubpanelComponent {
     id: number;
     private sub: any;
+    questions: Question[];
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute, private questionAnswerService: QuestionAnswerService) {
+        this.questions = this.questionAnswerService.getQuestionByCategory('');
+    }
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
             this.id = +params['id']; // (+) converts string 'id' to a number
+            this.questions = this.questionAnswerService.getQuestionByCategory(params['id']);
         });
     }
 
