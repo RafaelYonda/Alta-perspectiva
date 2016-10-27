a1af6038127df721a3e29ee9393935e77b60a64c
sayedur-nascenia <sayedur@nascenia.com>
2016-10-27 14:23:01 +0600

    question home page added.

---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/questions/question-home.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'js/app/questions/question-home.component.html'
+})
+export class QuestionHomeComponent {
+
+}
---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/questions/question.module.ts
@@ -0,0 +1,29 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+//====core components============
+
+import { QuestionHomeComponent } from './question-home.component';
+
+//================
+
+@NgModule({
+    imports: [BrowserModule, 
+        //RouterModule.forRoot([
+        //    { path: '', redirectTo: 'home', pathMatch:'full' },
+        //    { path: 'question', component: QuestionHome },
+        //    { path: 'learn', component: LearnHome },
+        //    { path: 'project', component: ProjectHome },
+        //])
+    ],
+    declarations: [
+        //Core Components
+    ],
+    exports: [
+        RouterModule,
+
+        //Core Components
+    ]
+})
+export class CoreModule { }
