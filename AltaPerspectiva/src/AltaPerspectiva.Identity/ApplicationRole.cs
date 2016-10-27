0f234e1a31d16661683683bf4b106cbf34ddba59
zafrulhassan <Zafrul@nascenia.com>
2016-10-27 20:03:22 +0600

    add openiddict

---
+++ AltaPerspectiva/src/AltaPerspectiva.Identity/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿    using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace AltaPerspectiva.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+    }
+}
