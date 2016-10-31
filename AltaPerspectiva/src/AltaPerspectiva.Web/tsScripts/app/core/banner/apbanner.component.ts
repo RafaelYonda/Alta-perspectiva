1816997dc2391e547da4af2ac98f8734669de8c0
Sharif Rifat <sharif@nascenia.com>
2016-10-31 20:43:30 +0600

    scrol top  added.. need to refactor the code.

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/banner/apbanner.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/core/banner/apbanner.component.ts
@@ -1,4 +1,5 @@
-﻿import { Component, HostListener, ElementRef } from '@angular/core';
+﻿import { Component, HostListener, ElementRef,Inject} from '@angular/core';
+
 
 @Component({
     selector: 'ap-banner',
@@ -10,12 +11,32 @@ export class ApBannerComponent {
     public heroes: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
     public images = IMAGES;
     private bannerHeight: Number = 200;
-    constructor(private _el: ElementRef) {
+    _window: any;
+    constructor(private _el: ElementRef, @Inject('Window') window: Window) {
+        this._window = window;
     }
 
     @HostListener('window:scroll', ['$event'])
     track(event) {
-        console.debug("Scroll Event", this._el.nativeElement.offsetParent.scrollTop);
+        //var doc = this._el.nativeElement.offsetParent;
+        var doc = this._el.nativeElement;
+        var nodeBanner = doc.childNodes[0];
+        console.debug(this._window.scrollY);
+        if (this._window.scrollY > 200) {
+            console.debug(this._el.nativeElement.parentNode);
+            console.debug(((this._el.nativeElement.parentNode).parentNode).parentNode);
+            console.debug(this._el.nativeElement.childNodes[1]);
+
+            this._el.nativeElement.parentNode.classList.add("fixed-top");
+            ((this._el.nativeElement.parentNode).parentNode).parentNode.classList.add("parent-padding");
+            this._el.nativeElement.childNodes[1].classList.add("fixed-height");
+        }
+        else {
+            this._el.nativeElement.parentNode.classList.remove("fixed-top");
+            ((this._el.nativeElement.parentNode).parentNode).parentNode.classList.remove("parent-padding");
+            this._el.nativeElement.childNodes[1].classList.remove("fixed-height");
+        }
+            
         // see http://juristr.com/blog/2016/01/learning-ng2-dynamic-styles/
     }
 }
