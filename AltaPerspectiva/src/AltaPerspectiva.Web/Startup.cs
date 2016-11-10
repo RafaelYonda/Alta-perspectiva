76c5c2980dd2318b73901827ade90ae0469a2fd1
zafrulhassan <Zafrul@nascenia.com>
2016-11-10 14:20:12 +0600

    Fix static files error

--- AltaPerspectiva/src/AltaPerspectiva.Web/Startup.cs
+++ AltaPerspectiva/src/AltaPerspectiva.Web/Startup.cs
@@ -54,6 +54,8 @@ namespace AltaPerspectiva
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
