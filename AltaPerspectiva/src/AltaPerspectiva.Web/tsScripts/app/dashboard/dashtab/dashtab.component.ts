c2e30c9800cbc4c556f7c5951c6cb6ec00efa1af
Sharif Rifat <sharif@nascenia.com>
2016-11-18 19:38:21 +0600

    dashboard  initial layut added

---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/dashtab/dashtab.component.ts
@@ -0,0 +1,39 @@
+﻿import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Http, Headers, Response } from '@angular/http';
+
+@Component({
+    selector: 'dash-tab',
+    encapsulation: ViewEncapsulation.None,
+    providers: [AuthenticationService],
+    templateUrl: 'js/app/dashboard/dashtab/dashtab.component.html',
+    styleUrls: ['js/app/dashboard/dashtab/dashtab.component.css'],
+})
+export class DashTab {
+    @Input() className: string;
+    isbackGround = false;
+    _logObj: any;
+    _authService: AuthenticationService;
+    
+    constructor(private authService: AuthenticationService) {
+        this._authService = authService;
+        this._logObj = authService.getLoggedinObj();
+    }
+    //LogIn() {
+    //    console.log("Log in");
+    //    this._authService.tempLogin();
+    //} LogOut() {
+    //    this._authService.logout();
+    //}
+}
+
+export class LogInObj {
+    notifyCount: number;
+    user: User;
+}
+export class User {
+    userid: number;
+    name: string;
+    occupassion: string;
+    imageUrl: string;
+}
