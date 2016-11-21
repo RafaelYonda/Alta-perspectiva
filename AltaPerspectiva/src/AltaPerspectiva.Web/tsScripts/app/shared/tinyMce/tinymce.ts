a7f7c1028602569ce47e57f32537bb0f6b1085d7
zafrulhassan <Zafrul@nascenia.com>
2016-11-21 21:03:53 +0600

    add tiny mce

---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/tinyMce/tinymce.ts
@@ -0,0 +1,40 @@
+﻿import {
+    Component,
+    OnDestroy,
+    AfterViewInit,
+    EventEmitter,
+    Input,
+    Output
+} from '@angular/core';
+
+declare var tinymce: any;
+@Component({
+    selector: 'alta-tiny-component',
+    template: `<textarea id="tinyid"></textarea>`
+})
+export class AltaTinyComponent implements AfterViewInit, OnDestroy {
+    @Input() elementId: String;
+    @Output() onEditorKeyup = new EventEmitter<any>();
+
+    editor;
+
+    ngAfterViewInit() {
+        tinymce.init({
+            selector: '#tinyid',
+            plugins: ['link', 'paste', 'table','image'],
+            skin_url: 'Scripts/assets/skins/lightgray',
+            toolbar1: 'undo redo | styleselect | bold italic | link image',
+            setup: editor => {
+                this.editor = editor;
+                editor.on('keyup', () => {
+                    const content = editor.getContent();
+                    this.onEditorKeyup.emit(content);
+                });
+            },
+        });
+    }
+
+    ngOnDestroy() {
+        tinymce.remove(this.editor);
+    }
+}
