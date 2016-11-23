a2a81c13c152d2d8ae21094b0bd6eb5fe1cbe33d
Sharif Rifat <sharif@nascenia.com>
2016-11-23 18:04:59 +0600

    Dashboard profile routing added

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/dashboard.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/dashboard.component.ts
@@ -2,6 +2,12 @@
 
 @Component({
     templateUrl: 'js/app/dashboard/dashboard.component.html'
+    //styleUrls: ['js/app/dashboard/dashboard.component.css',
+    //    'js/app/dashboard/styles/dashboard.buttons.css',
+    //    'js/app/dashboard/styles/dashboard.forms.css',
+    //    'js/app/dashboard/styles/dashboard.profile.css',
+    //    'js/app/dashboard/styles/dashboard.progress.css',
+    //    'js/app/dashboard/styles/dashboard.status.css'],
 })
 export class DashBoardComponent {
 }
--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/dashboard.module.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/dashboard.module.ts
@@ -1,19 +1,45 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { ModuleWithProviders }  from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { DashBoardComponent } from './dashboard.component';
+import { EditProfileComponent } from './eidtprofile/editprofile.component';
+import { ContactFormComponent } from './eidtprofile/profilesforms/contact.component';
+import { BiographyFormComponent } from './eidtprofile/profilesforms/biography.component';
 import { NavDashboard } from './navdashboard/navdashboard.component';
 import { DashTab } from './dashtab/dashtab.component';
 
+
+const DashBoardroutes: Routes = [
+    {
+        path: 'dashboard', component: DashBoardComponent,
+        children: [
+            { path: '', redirectTo: 'editprofile' },
+            {
+                path: 'editprofile', component: EditProfileComponent,
+                children: [
+                    { path: '', redirectTo: 'contact' }, 
+                    { path: 'contact', component: ContactFormComponent },
+                    { path: 'biography', component: BiographyFormComponent },
+                ]
+            }
+        ]
+    }
+];
+export const dashboardRouting: ModuleWithProviders = RouterModule.forRoot(DashBoardroutes);
+
+
 @NgModule({
-    imports: [BrowserModule, RouterModule.forRoot([
-        { path: 'dashboard', component: DashBoardComponent }])
+    imports: [BrowserModule, dashboardRouting
+        //RouterModule.forRoot([
+        //{ path: 'dashboard', component: DashBoardComponent }])
     ],
     declarations: [
-        DashBoardComponent, NavDashboard, DashTab
+        DashBoardComponent, EditProfileComponent, NavDashboard, DashTab, ContactFormComponent, BiographyFormComponent
     ],
-    exports: [
+    exports: [DashBoardComponent
     ]
 })
 export class DashBoardModule { }
+
