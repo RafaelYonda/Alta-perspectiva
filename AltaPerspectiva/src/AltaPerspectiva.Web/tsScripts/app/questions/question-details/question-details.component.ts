e80981d91d887d5c9f0fe7d2c0cdaa5d75897b52
Sharif Rifat <sharif@nascenia.com>
2016-11-24 15:46:05 +0600

    Question files added only answer button design

---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/questions/question-details/question-details.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionAnswerService } from '../../services/questionAnswer.service';
+import {QuestionMenu, Answer, DateName} from '../../services/models';
+
+@Component({
+    selector: "question-details",
+    templateUrl: 'js/app/questions/question-details/question-details.component.html',
+    styleUrls: ['js/app/questions/question-details/question-details.css'],
+    providers: [QuestionAnswerService]
+})
+export class QuestionPanelComponent {
+    date: DateName;
+    id: number;
+    private sub: any;
+    answerList: Answer[];
+    constructor(questionService: QuestionAnswerService) {
+        this.answerList = questionService.getAnswersByQuestion(2);
+        console.log(this.answerList);
+        this.date= new DateName();
+    }
+}
