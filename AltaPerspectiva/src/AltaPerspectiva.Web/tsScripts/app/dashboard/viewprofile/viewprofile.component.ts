1fdc83f06bc844b2ed92dc5887ca56fde0326412
Sharif Rifat <sharif@nascenia.com>
2016-12-20 19:31:20 +0600

    View profile only component added

---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/viewprofile.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+import { ImageUploadService } from '../../services/image-upload.service';
+import { Profile } from '../../services/models';
+@Component({
+    templateUrl: 'js/app/dashboard/viewprofile/viewprofile.component.html',
+    //styleUrls: ['js/app/dashboard/eidtprofile/editprofile.component.css',
+    //    'js/app/dashboard/styles/dashboard.buttons.css',
+    //    'js/app/dashboard/styles/dashboard.forms.css',
+    //    'js/app/dashboard/styles/dashboard.profile.css',
+    //    'js/app/dashboard/styles/dashboard.progress.css',
+    //    'js/app/dashboard/styles/dashboard.status.css'],
+    providers: [ImageUploadService]
+})
+export class ViewProfileComponent {
+
+}
