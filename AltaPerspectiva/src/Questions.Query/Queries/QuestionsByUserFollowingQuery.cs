3d63fbabc507565ed08053094ca94bd2231e75a1
zafrulhassan <Zafrul@nascenia.com>
2016-12-22 19:35:27 +0600

    category follow query

---
+++ AltaPerspectiva/src/Questions.Query/Queries/QuestionsByUserFollowingQuery.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Questions.Domain;
+using AltaPerspectiva.Core.Infrastructure;
+using Questions.Query.DbContext;
+using System.Threading.Tasks;
+using System;
+
+namespace Questions.Query
+{
+	public class QuestionsByUserFollowingQuery : EFQueryBase<QuestionsQueryDbContext>, IQuestionsByUserFollowingQuery
+    {
+		public QuestionsByUserFollowingQuery(QuestionsQueryDbContext context)
+			: base(context)
+		{
+		}
+
+        public async Task<IEnumerable<Question>> Execute(Guid UserId)
+        {
+            var categories = DbContext.CategoryFollowers.Where(c => c.UserId == UserId).Select(x=>x.CategoryId).ToList();
+
+            return await DbContext.
+                                Questions
+                                    .Include(q=>q.Categories)
+                                        .ThenInclude(c=>c.Category)
+                                            .Where(q => q.Categories.Any(x => categories.Contains( x.CategoryId)))
+                                            .OrderByDescending(c => c.CreatedOn.Value.Date)
+                                                .ThenByDescending(c => c.CreatedOn.Value.TimeOfDay)
+                                                    .Take(20)
+                                                        .ToListAsync();
+        }
+    }
+}
