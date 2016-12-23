36c82f72d3227ec36daf19f1096ed721724a093a
zafrulhassan <Zafrul@nascenia.com>
2016-12-23 11:19:53 +0600

    add category follower

--- DB Scripts/CreateQuestion.sql
+++ DB Scripts/CreateQuestion.sql
@@ -230,3 +230,19 @@ GO
 USE [master]
 GO
 
+CREATE TABLE [Questions].[CategoryFollowers](
+	[Id] [uniqueidentifier] NOT NULL,
+	[CategoryId] [uniqueidentifier] NOT NULL,
+	[CreatedBy] [uniqueidentifier] NULL,
+	[CreatedOn] [datetime2](7) NULL,
+	[DTS] [datetime2](7) NOT NULL,
+	[IsActive] [bit] NULL,
+	[IsDeleted] [bit] NULL,
+	[ModifiedBy] [uniqueidentifier] NULL,
+	[ModifiedOn] [datetime2](7) NULL,
+	[UserId] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_CategoryFollowers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
