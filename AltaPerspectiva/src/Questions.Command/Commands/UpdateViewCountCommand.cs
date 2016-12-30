d0aea0bf73dde18697f066c58ef5e958419f946d
zafrulhassan <Zafrul@nascenia.com>
2016-12-30 12:46:08 +0600

    add command handler for viewcount

---
+++ AltaPerspectiva/src/Questions.Command/Commands/UpdateViewCountCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Questions.Command
+{
+    using System;
+    using AltaPerspectiva.Core;
+    using AltaPerspectiva.Identity;
+    using System.Collections.Generic;
+    using Domain;
+
+    public class UpdateViewCountCommand : ICommand
+    {
+        public UpdateViewCountCommand(Guid? _questionId)
+        {   
+            QuestionId = _questionId;           
+        }
+        public Guid Id { get; set; }        
+        public Guid? QuestionId { get; set; }       
+    }
+}
+
