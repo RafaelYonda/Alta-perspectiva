003a9f5b8d45bdb2b986ee4323611cbde38af8a1
Sharif Rifat <sharif@nascenia.com>
2017-01-09 20:14:20 +0600

    Design changs for user info

---
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/user-info/user-info.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component, Input } from '@angular/core';
+import { CommentService } from '../../services/comment.service';
+import {User, Comment } from '../../services/models';
+
+@Component({
+    selector: 'user-info',
+    templateUrl: 'js/app/shared/user-info/user-info.component.html',
+    styleUrls: ['js/app/shared/user-info/user-info.component.css'],
+    providers: [CommentService]
+})
+export class UserInfoComponent {
+    commentText: string;
+    comment: Comment;
+    comments: Comment[];
+    userObj: User;
+    @Input() user: any;
+    @Input() viewDate: Date;
+    constructor() {
+        this.userObj = this.user;
+        console.log(this.user);
+    }
+}
