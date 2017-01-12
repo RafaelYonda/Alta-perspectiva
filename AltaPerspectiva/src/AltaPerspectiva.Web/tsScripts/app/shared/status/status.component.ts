fa7227bd5cef5f5625537902f0a395628ff74c60
zafrulhassan <Zafrul@nascenia.com>
2017-01-12 20:10:24 +0600

    place ap status in question details page

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/status/status.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/status/status.component.ts
@@ -15,48 +15,22 @@ export class StatusComponent {
     likedUsers: any;
     constructor(private statusService: StatusService,private dataService: QuestionAnswerService) {
     }
-    submitLike(answerId: string,questionId:string) {
-        console.log(answerId+'->'+questionId);
-       // this.like = new Like();
-       // this.like.questionId = questionId;
-      //  this.statusService.addQuestionLike(this.like).subscribe(res => {
-      //      this.like.userId = res.userId;
-       //     var douplicateLike = this.questionObj.likes.find(x => x.userId == res.userId && x.questionId == questionId);
-       //     if (douplicateLike == undefined) {
-        //        this.questionObj.likes.push(this.like);
-         //   }
-       // });
+    submitLike(answerId: string,questionId:string) {         
         this.like = new Like();
         this.like.answerId = answerId;
         this.like.questionId = questionId;
 
         this.dataService.addAnswerLike(this.like).subscribe(res => {
-            this.like.userId = res.userId ;    
-             console.log(res);
+            this.like.userId = res.userId ;       
             var douplicateLike = this.questionObj.answers[0].likes.find(x => x.answerId == answerId);
             if (douplicateLike == undefined) {
                     this.questionObj.answers[0].likes.push(this.like);
-               }
-            //    console.log(this.question.answers.find(x => x.id == answerId).likes);
-            //   var douplicateLike= this.question.answers.find(x => x.id == answerId).likes.find(x => x.userId == res.userId &&x.answerId==answerId);
-            //  console.log(douplicateLike);
-            //if (douplicateLike==undefined) {
-            //   this.question.answers.find(x=>x.id==answerId).likes.push(this.like);
-            //}
-
+            }
         });
     }
-    submitLikeUserDetails(answerId: string,questionId:string) {
-        console.log(answerId + '->' + questionId);
-        this.statusService.addLikeUserDetailsByAnswer(answerId,questionId).subscribe(res => {
-            console.log(res);
+    submitLikeUserDetails(answerId: string,questionId:string) {       
+        this.statusService.addLikeUserDetailsByAnswer(answerId,questionId).subscribe(res => {           
             this.likedUsers = res;
-        });
-        // this.like = new Like();
-        // this.like.questionId = questionId;       
-
-        //  this.questionAnswerService.addQuestionLike(this.like).subscribe(res => {
-        //    this.questions.find(x=>x.id==questionId).likes.push(this.like);
-        //  });
+        });       
     }
 }
