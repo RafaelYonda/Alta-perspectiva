cdcd20e5d6104d0271390822d25e4633b89e6765
zafrulhassan <Zafrul@nascenia.com>
2017-01-13 21:17:44 +0600

    add comments query by answer

---
+++ AltaPerspectiva/src/Questions.Query/Queries/AnswerCommentsQuery.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Questions.Domain;
+using AltaPerspectiva.Core.Infrastructure;
+using Questions.Query.DbContext;
+using System.Threading.Tasks;
+using System;
+
+namespace Questions.Query
+{
+	public class AnswerCommentsQuery : EFQueryBase<QuestionsQueryDbContext>, IAnswerCommentsQuery
+    {
+		public AnswerCommentsQuery(QuestionsQueryDbContext context)
+			: base(context)
+		{
+		}
+
+        public async Task<IEnumerable<Comment>> Execute(Guid AnswerId)
+        {
+              return await DbContext.Comments                                    
+                                    .Where(c=> c.AnswerId == AnswerId)                                  
+                                    .ToListAsync();
+        }
+    }
+}
---
+++ AltaPerspectiva/src/Questions.Query/Queries/IAnswerCommentsQuery.cs
@@ -0,0 +1,13 @@
+﻿using AltaPerspectiva.Core;
+using Questions.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Questions.Query
+{
+    public interface IAnswerCommentsQuery : IQuery
+    {
+        Task<IEnumerable<Comment>> Execute(Guid AnswerId);
+    }
+}
