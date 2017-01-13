1b27432b90e9fbe850baff8cd8949739ebeb0456
sharifnascenia <sharif@nascenia.com>
2017-01-13 15:42:59 +0600

    alter stored procedure is added

---
+++ DB Scripts/alter stored procedure.sql
@@ -0,0 +1,61 @@
+ALTER proc [dbo].[SpTopUserCalculation]
+(
+  @userId nvarchar(200)=null,
+  @catgeoryId nvarchar(200)=null
+)
+as
+BEGIN
+--for Top Five user
+if (@userId is null)
+	BEGIN
+	;with CTE
+	as(
+	SELECT  [Id]
+	,[UserName],
+	(select top 1 FirstName+' '+LastName as FullName from UserProfile.ContractInformation where UserId=u.Id order by Id desc) FullName,
+	(select top 1 Image as FullName from UserProfile.UserImage where UserId=u.Id order by Id desc) ImageUrl,
+	(select ISNULL(count(*),0) TotalLike from Questions.Likes where UserId=u.Id) TotalLike,
+	(select ISNULL(count(*),0) TotalComment from Questions.Comments  where UserId=u.Id) TotalComment,
+	(select ISNULL(count(*),0) TotalQuestion from Questions.Questions where UserId=u.Id) TotalQuestion,
+	(select ISNULL(count(*),0) TotalAnswer from Questions.Answers where UserId=u.Id) TotalAnswer,
+	(
+	 (select ISNULL(count(*),0) TotalLike from Questions.Likes where UserId=u.Id)*1+
+	 (select ISNULL(count(*),0) TotalComment from Questions.Comments  where UserId=u.Id) *2+
+	 (select ISNULL(count(*),0) TotalQuestion from Questions.Questions where UserId=u.Id) *3+
+	 (select ISNULL(count(*),0) TotalAnswer from Questions.Answers where UserId=u.Id) *4
+	) TotalCommulativePoint
+	
+	FROM [Identity].AspNetUsers u
+
+	)
+	select top 5 * from CTE order by TotalCommulativePoint desc
+	END
+	---top user by category Id
+ELSE IF (@userId is null and @catgeoryId is not null)
+BEGIN
+select * from UserProfile.Biography;
+END
+
+ELSE
+	BEGIN
+	SELECT  [Id]
+	,[UserName],
+	(select top 1 FirstName+' '+LastName as FullName from UserProfile.ContractInformation where UserId=u.Id order by Id desc) FullName,
+	(select top 1 Image as FullName from UserProfile.UserImage where UserId=u.Id order by Id desc) ImageUrl,
+	(select ISNULL(count(*),0) TotalLike from Questions.Likes where UserId=u.Id) TotalLike,
+	(select ISNULL(count(*),0) TotalComment from Questions.Comments  where UserId=u.Id) TotalComment,
+	(select ISNULL(count(*),0) TotalQuestion from Questions.Questions where UserId=u.Id) TotalQuestion,
+	(select ISNULL(count(*),0) TotalAnswer from Questions.Answers where UserId=u.Id) TotalAnswer,
+	(
+	 (select ISNULL(count(*),0) TotalLike from Questions.Likes where UserId=u.Id)*1+
+	 (select ISNULL(count(*),0) TotalComment from Questions.Comments  where UserId=u.Id) *2+
+	 (select ISNULL(count(*),0) TotalQuestion from Questions.Questions where UserId=u.Id) *3+
+	 (select ISNULL(count(*),0) TotalAnswer from Questions.Answers where UserId=u.Id) *4
+	) TotalCommulativePoint
+	
+	FROM [Identity].AspNetUsers u
+	where u.Id=@userId
+	END
+
+END
+Go
