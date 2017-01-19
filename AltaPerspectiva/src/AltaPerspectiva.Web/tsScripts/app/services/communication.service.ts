853a03bacae7151180882dc64e05ee47bf8a1eb0
Sharif Rifat <sharif@nascenia.com>
2017-01-19 19:40:36 +0600

    Home page: spacing fixed for  left panel, tab panel css moved to style page.

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/services/communication.service.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/services/communication.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CommunicationService {
-
+    //========Category Change==========
     private categoryId: string;
     private objChange: Subject<string> = new Subject<string>();
 
@@ -15,5 +15,19 @@ export class CommunicationService {
 
     getCategory(): Observable<string> {        
         return this.objChange.asObservable();
+    }
+
+    //===========Question Change==
+    private questionId: string;
+    private questionChange: Subject<string> = new Subject<string>();
+
+    setQuestionId(questionId: string): void {
+        console.log('Question Id: ', questionId);
+        this.questionId = questionId;
+        this.questionChange.next(questionId);
+    }
+
+    getQuestionId(): Observable<string> {
+        return this.questionChange.asObservable();
     }
 }
