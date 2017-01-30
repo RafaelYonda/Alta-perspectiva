177a1e14068f6addb858a70095e6fc807a5b67db
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-01-30 16:28:52 +0600

    akash:Qquestion report added with all component

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/question-panel/question-panel.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/question-panel/question-panel.component.ts
@@ -1,16 +1,23 @@
 ﻿import { Component, Input, ViewContainerRef, ComponentFactoryResolver, ViewChild } from '@angular/core';
-import { Question } from '../../services/models';
+import { Question, QuestionReport } from '../../services/models';
 import { AnswerDialogComponent } from '../answer-dialog/answer-dialog.component';
 import { DialogComponent } from '../dialog-modal/dialog.component';
 
+import { QuestionAnswerService } from '../../services/question-answer.service';
 
+//QuestionReport
+import { QuestionReportComponent } from '../../shared/question-report/question-report.component';
 @Component({
     selector: 'question-panel',
     templateUrl: 'js/app/shared/question-panel/question-panel.component.html',
+    providers: [QuestionAnswerService]
 })
 export class QuestionPanelComponent {
     @Input() question: Question;
-    constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+    //Question report
+    questionReports: QuestionReport[];
+    constructor(private componentFactoryResolver: ComponentFactoryResolver, private dataService: QuestionAnswerService) { }
 
     @ViewChild('answerAnchor', { read: ViewContainerRef }) answerAnchor: ViewContainerRef;
     answerDialogBox(question: Question) {
@@ -33,6 +40,28 @@ export class QuestionPanelComponent {
         let dialogComponentFactory = this.componentFactoryResolver.resolveComponentFactory(DialogComponent);
         let dialogComponentRef = this.dialogAnchor.createComponent(dialogComponentFactory);
         dialogComponentRef.instance.question = question; // Not sure about the translation here
+        dialogComponentRef.instance.close.subscribe(() => {
+            dialogComponentRef.destroy();
+        });
+    }
+
+    @ViewChild('questionReport', { read: ViewContainerRef }) questionReport: ViewContainerRef;
+    onQuestionReportClicked(showQuestionReportModal: any) {
+        //console.log('onQuestionReportClicked->onQuestionReportClicked->onQuestionReportClicked->onQuestionReportClicked');
+        console.log(showQuestionReportModal);
+        this.questionReport.clear();
+        let dialogComponentFactory = this.componentFactoryResolver.resolveComponentFactory(QuestionReportComponent);
+        let dialogComponentRef = this.questionReport.createComponent(dialogComponentFactory);
+
+        this.dataService.GetReport().subscribe(res => {
+            this.questionReports = res;
+            dialogComponentRef.instance.questionReports = this.questionReports;
+            dialogComponentRef.instance.questionId = showQuestionReportModal.questionId;
+            dialogComponentRef.instance.answerId = showQuestionReportModal.answerId;
+
+        })
+
+
         dialogComponentRef.instance.close.subscribe(() => {
             dialogComponentRef.destroy();
         });
