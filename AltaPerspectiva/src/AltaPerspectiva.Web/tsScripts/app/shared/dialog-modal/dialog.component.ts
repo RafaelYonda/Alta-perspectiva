224dae3adc35ac934bb98bbffb2d0749f93102df
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-02-02 18:20:33 +0600

    akash:Dialog with popu adn route added

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/dialog-modal/dialog.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/dialog-modal/dialog.component.ts
@@ -1,17 +1,28 @@
 ﻿import { Component, EventEmitter } from '@angular/core';
 import { Question} from '../../services/models';
-
+import { QuestionAnswerService } from '../../services/question-answer.service';
+import { Router } from '@angular/router';
 @Component({
     selector: 'dlg',
     templateUrl: 'js/app/shared/dialog-modal/dialog.component.html',
     styleUrls: ['js/app/shared/dialog-modal/dialog.component.css'],
+    providers: [QuestionAnswerService]
 })
 export class DialogComponent {
     close = new EventEmitter();
     question: Question;
+    //QuestionEditModal
 
+    //Edit question popup
+    showQuestionEditForm: boolean
+    editTitle: string
+    editBody: string
+    _router: Router;
     onClickedExit() {
         this.close.emit('event');
+    }
+    constructor(private dataService: QuestionAnswerService, router: Router) {
+        this._router = router;
     }
     ngOnInit() {
         console.log('My question'+this.question);
@@ -23,4 +34,26 @@ export class DialogComponent {
         if (value =='dialogModal')
             this.close.emit('event');
     }
+    //Edit Question Popup
+    onQuestionDetailClicked(showEditForm: boolean) {
+        this.showQuestionEditForm = showEditForm;
+        this.editTitle = this.question.title;
+        this.editBody = this.question.body;
+    }
+    updateQuestion() {
+        var qv = new Question();
+        qv.title = this.editTitle;
+        qv.body = this.editBody;
+        qv.id = this.question.id;
+
+        this.dataService.updateQuestion(qv).subscribe(res => {
+            this.question.title = this.editTitle;
+            this.question.body = this.editBody;
+            this.showQuestionEditForm = false;
+            this.editTitle = '';
+            this.editTitle='';
+            this._router.navigateByUrl('question/home/1', { skipLocationChange: true })
+        });
+    }
+
 }
