1aaf2a492eb7cc5a8c2743e375826bdc44e155c4
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-02-03 17:13:04 +0600

    AKAHS:SOME COMMIT

--- AltaPerspectiva/src/Questions.Domain/QuestionLevel.cs
+++ AltaPerspectiva/src/Questions.Domain/QuestionLevel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace Questions.Domain
         [Key]
         public Int64 Id { get; set; }
         public Guid QuestionId { get; set; }
+
+        [NotMapped]
         public virtual Question Question { get; set; }
 
         public Guid? LevelId { get; set; }
