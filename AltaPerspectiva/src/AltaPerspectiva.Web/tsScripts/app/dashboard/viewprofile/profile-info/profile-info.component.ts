250a7dfef3eca59f45dafa59774a3084452371a5
Sharif Rifat <sharif@nascenia.com>
2017-02-09 12:19:51 +0600

    User name text change html only added

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/profile-info/profile-info.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/profile-info/profile-info.component.ts
@@ -1,6 +1,39 @@
 ﻿import { Component } from '@angular/core';
+import { ImageUploadService } from '../../../services/image-upload.service';
+import { ConfigService } from '../../../services/config.service';
+import { ProfileService } from '../../../services/profile.service';
 @Component({
     selector: 'profile-info',
-    templateUrl: 'js/app/dashboard/viewprofile/profile-info/profile-info.component.html'
+    templateUrl: 'js/app/dashboard/viewprofile/profile-info/profile-info.component.html',
+    providers: [ImageUploadService, ConfigService]
 })
-export class ProfileInfoComponent { }
+export class ProfileInfoComponent {
+    username='Rafael Yonda'
+    isHidden = true;
+    imageLink: string;
+    constructor(private _imgService: ImageUploadService, private _configService: ConfigService, private profileService: ProfileService) {
+    }
+    ngOnInit() {
+        this._configService.getConfig().subscribe(res => {      //Get config for image
+            this.imageLink = res.profileImage;
+            var user = this.profileService.GetUser().subscribe(usr => {     //Get User Image
+                console.log(usr);
+                if (usr.imageUrl&&(usr.imageUrl != ''))
+                    this.imageLink += '/' + usr.imageUrl;
+                else this.imageLink = '../images/userAdd.png';
+            });
+        });
+    }
+    onChange(event) {
+        let file = event.srcElement.files;
+        //Upload the image 
+        this._imgService
+            .upload(file)
+            .subscribe(res => {
+                this.ngOnInit();
+            });
+    }
+    UpdateUserName() {
+        console.log(this.username);
+    }
+}
