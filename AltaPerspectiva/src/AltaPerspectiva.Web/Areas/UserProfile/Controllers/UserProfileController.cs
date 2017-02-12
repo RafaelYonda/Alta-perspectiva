28e3e3efdf90c691870615233d37a6c807ce4a3b
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-02-12 20:15:44 +0600

    aaaa

--- AltaPerspectiva/src/AltaPerspectiva.Web/Areas/UserProfile/Controllers/UserProfileController.cs
+++ AltaPerspectiva/src/AltaPerspectiva.Web/Areas/UserProfile/Controllers/UserProfileController.cs
@@ -101,8 +101,8 @@ namespace AltaPerspectiva.Web.Areas.UserProfile.Controllers
             Guid loggedinUser = new Guid("9f5b4ead-f9e7-49da-b0fa-1683195cfcba");
             if (User.Identity.IsAuthenticated)
             {
-                var userId = User.Claims.Where(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Select(x => x.Value);
-                loggedinUser = new Guid(userId?.ElementAt(0).ToString());
+                var uid = User.Claims.Where(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Select(x => x.Value);
+                loggedinUser = new Guid(uid?.ElementAt(0).ToString());
 
             }
             Credential credential = queryFactory.ResolveQuery<ICredentialQuery>().GetCredential(userId);
