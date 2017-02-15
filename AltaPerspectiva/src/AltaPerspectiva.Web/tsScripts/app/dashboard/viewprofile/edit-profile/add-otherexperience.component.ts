7d7eb2260bf0efb3ea4b6d164961c64eb06fc4e0
zafrulhassan <Zafrul@nascenia.com>
2017-02-15 21:42:50 +0600

    add categories dropdown

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/edit-profile/add-otherexperience.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/edit-profile/add-otherexperience.component.ts
@@ -1,15 +1,26 @@
 ﻿import { Component, EventEmitter } from '@angular/core';
 import { OtherExperience } from '../../../services/models/models.OtherExperience';
+import { Category} from '../../../services/models';
 import { ProfileService } from '../../../services/profile.service';
+import { CategoryService } from '../../../services/category.service';
 @Component({
     selector: 'add-otherexperience',
     templateUrl: 'js/app/dashboard/viewprofile/edit-profile/add-otherexperience.component.html',
-    providers: [ProfileService],
+    providers: [ProfileService, CategoryService],
 })
 export class AddOtherExperienceComponent {
     otherExperience: OtherExperience = new OtherExperience();
-    constructor(private profileService: ProfileService) {
+    categories: Category[];
+
+    constructor(private profileService: ProfileService, private categoryService: CategoryService) {
     }
+    
+    ngOnInit() {
+        this.categoryService.getAllCategories().subscribe(res => {
+            this.categories = res;
+        });
+    }
+
     close = new EventEmitter();
 
     onClickedExit() {
@@ -22,6 +33,9 @@ export class AddOtherExperienceComponent {
         if (value == 'dialogModal')
             this.close.emit('event');
     }
+
+   
+
     saveOtherExperience() {
         console.log(this.otherExperience);
     }
