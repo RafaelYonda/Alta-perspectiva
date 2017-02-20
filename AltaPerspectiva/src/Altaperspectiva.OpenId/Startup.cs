6d0957ab02fe43b4c040d5a3f56b320b3b3b4258
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-02-20 14:01:01 +0600

    some chamnegs

--- AltaPerspectiva/src/Altaperspectiva.OpenId/Startup.cs
+++ AltaPerspectiva/src/Altaperspectiva.OpenId/Startup.cs
@@ -116,7 +116,7 @@ namespace Altaperspectiva.OpenId {
                 options.AutomaticAuthenticate = true;
                 options.AutomaticChallenge = true;
                 options.Authority = "http://localhost:54540/";
-                options.Audience = "resource_server";
+            //    options.Audience = "resource_server";
                 options.ClientId = "resource_server";
                 options.ClientSecret = "875sqd4s5d748z78z7ds1ff8zz8814ff88ed8ea4z4zzd";
             });
