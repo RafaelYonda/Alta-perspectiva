12d7a650fd46370eacf2939e657d91f9247b2972
Sharif Rifat <sharif@nascenia.com>
2017-03-14 19:48:07 +0600

    minor fixes for search panel

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/search/apSearch.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/shared/search/apSearch.component.ts
@@ -128,7 +128,7 @@ export class ApSearchComponent {
         console.log(textWidth);
         var key = event.keyCode || event.charCode;
         textWidth = (textWidth + 20) < 30 ? (textWidth + 10) : (textWidth + 7);
-        if (textWidth > 635)
+        if (textWidth > 642)
             return;
         console.log(textWidth);
         //==============
