948a5bccd5982cc2d4b7967b9d176cd24955e59e
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-03-20 17:25:05 +0600

    akash:Blog post comment bug fixed with backend bug fixed

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/blog-post/post-comment.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/blog-post/post-comment.component.ts
@@ -1,9 +1,10 @@
 ﻿import { Component, Input, EventEmitter, Output, ViewContainerRef, ComponentFactoryResolver, ViewChild } from '@angular/core';
 import { BlogService } from '../../../services/blog.service';
+import { ProfileService } from '../../../services/profile.service';
 import {LogInObj, User } from '../../../services/models';
 import { BlogPost, BlogLike, BlogComment } from '../../../services/models/models.blogpost';
 import { loginModalComponent } from '../../../shared/login-modal/login-modal.component';
-
+import { CredentialViewModel }     from '../../../services/models/models.profile';
 @Component({
     selector: 'blogpost-comment',
     templateUrl: 'js/app/dashboard/viewprofile/blog-post/post-comment.component.html',
@@ -17,12 +18,12 @@ export class BlogpostCommentComponent {
     comments: BlogComment[];
 
     _logObj: LogInObj;
-
+    imageLink:string;
     @Input() blogPost: BlogPost;
 
     @Output() sendCommentCount = new EventEmitter<any>();
 
-    constructor(private commentService: BlogService, private componentFactoryResolver: ComponentFactoryResolver) {
+    constructor(private commentService: BlogService, private componentFactoryResolver: ComponentFactoryResolver, private profileService: ProfileService) {
         //var user: User = new User();
         //user.userId = '-1';
         //this._logObj = { isLoggedIn: false, user: user };
@@ -34,10 +35,11 @@ export class BlogpostCommentComponent {
         this._logObj = localStorage.getItem('currentUserObject') as any;
         if (!this._logObj)
             this._logObj = new LogInObj();
-        //if (currentUserName != null) {
-        //    this._logObj.user.name = currentUserName;
-        //    this._logObj.user.imageUrl = currentUserImage;
-        //}
+        if (currentUserName != null) {
+            this.imageLink = localStorage.getItem('urrentUserImage');
+        } else {
+            this.imageLink = '../../images/avatar.png';
+        }
     }
 
     submitComment(blogPostId: string) {
