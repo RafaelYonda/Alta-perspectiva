eeb0456e1849d3c4af1ee82471a08c9e50cb0ae6
Sharif Rifat <sharif@nascenia.com>
2017-04-03 16:25:43 +0600

    Responsive nav bar spanish text changed, nav bar fixed for full screen

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/questions/question-fullscreen/question-fullscreen.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/questions/question-fullscreen/question-fullscreen.component.ts
@@ -2,7 +2,7 @@
 import { QuestionAnswerService } from '../../services/question-answer.service';
 import { CategoryService } from '../../services/category.service';
 import { ConfigService } from '../../services/config.service';
-import {QuestionMenu, Question, Answer, Category, Like, DateName, TotalCount, Config, LogInObj} from '../../services/models';
+import {QuestionMenu, Question, Answer, Category, Like, DateName, TotalCount, Config,User, LogInObj} from '../../services/models';
 import { Router, ActivatedRoute, Resolve } from '@angular/router';
 import { CommunicationService } from '../../services/communication.service';
 import { QuestionResolver } from '../../services/resolve.services/question.resolver';
@@ -18,14 +18,33 @@ import { AnswerDialogComponent } from '../../shared/answer-dialog/answer-dialog.
 export class QuestionFullscreenComponent {
     question: Question;
     route: any;
+	_logObj: LogInObj;
     constructor(private router: Router, private _route: ActivatedRoute) {
         this.route = _route;
+		var user: User = new User();
+		this._logObj = { isLoggedIn: false, user: user };
     }
     ngOnInit() {
+	window.scrollTo(0, 0);
+    var currentUserName = localStorage.getItem('auth_token'); 
+    var currentUserImage = localStorage.getItem('currentUserImage');
+	if (currentUserName != null)
+        {
+            this._logObj.user.name = currentUserName;
+            this._logObj.user.imageUrl = currentUserImage;
+        }
         this.route.data
             .subscribe(res => {
                 this.question = res.question;
                 console.log(this.question);
                 });
+    }
+
+	    hideMenu() {
+        var leftMenu = document.getElementById('toggleMenu');
+        leftMenu.classList.remove("expand");
+    }
+    gotoProfile() {
+        this.router.navigateByUrl('/dashboard/viewprofile/' + this._logObj.user.userId + '/user-question');
     }
 }
