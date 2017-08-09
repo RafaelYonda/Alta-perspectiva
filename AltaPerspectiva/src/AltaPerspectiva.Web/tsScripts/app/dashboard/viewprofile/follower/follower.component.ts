9697b9b867ce373213a7e1b92378b3d6d7a35736
Sharif Rifat <sharif@gmail.com>
2017-08-09 15:30:47 +0600

    dash board : show loader on question and answer loading

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/follower/follower.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/dashboard/viewprofile/follower/follower.component.ts
@@ -8,19 +8,28 @@ import { ActivatedRoute } from '@angular/router';
 export class FollowerComponent {
     private sub: any;
     user: User[];
+    isLoading = false;
     constructor(private _route: ActivatedRoute,private profileServ: ProfileService) {
         
     }
     ngOnInit() {
+        this.showLoader();
         window.scrollTo(0, 0);
         this.sub = this._route.parent.params.subscribe(params => {
             this.profileServ.GetFollowersByUserId(params['userId']).subscribe(usr => {     //Get User Image
                 this.user = usr;
                 console.log(this.user);
+                this.hideLoader();
             });
         });
     }
     ngOnDestroy() {
         this.sub.unsubscribe();
+    }
+    showLoader() {
+        this.isLoading = true;
+    }
+    hideLoader() {
+        this.isLoading = false;
     }
 }
