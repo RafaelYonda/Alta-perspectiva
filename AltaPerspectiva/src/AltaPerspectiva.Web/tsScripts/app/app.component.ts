b4ed7e427f5d0a1907b59b0b9c15e4aebb3db007
Sharif Rifat <sharif@gmail.com>
2017-08-21 16:27:18 +0600

    User log out after some time fixed by continuous request

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/app.component.ts
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/app/app.component.ts
@@ -8,17 +8,42 @@ import { ToastsManager, ToastOptions } from 'ng2-toastr/ng2-toastr';
     providers: [AuthenticationService]
 
 })
-export class AppComponent {   
+export class AppComponent {
+    username: string;
+    token: string
 
     constructor(elm: ElementRef, private _authService: AuthenticationService, public toastr: ToastsManager, vRef: ViewContainerRef) {
         this.toastr.setRootViewContainerRef(vRef);
 
-        var username = elm.nativeElement.getAttribute('username');
-        var token = elm.nativeElement.getAttribute('token');       
-       
-        if (username && username != "") {
-            localStorage.setItem('currentUser', username);
-            localStorage.setItem('auth_token', token);
+        this.username = elm.nativeElement.getAttribute('username');
+        this.token = elm.nativeElement.getAttribute('token');       
+
+        this.setLocalStorage();
+        
+        //if (username && username != "") {
+        //    localStorage.setItem('currentUser', username);
+        //    localStorage.setItem('auth_token', token);
+        //    this._authService.getLoggedinObj().subscribe(res => {
+        //        localStorage.setItem('currentUserId', res.userId);
+        //        localStorage.setItem('currentUserName', res.name);
+        //        localStorage.setItem('currentUserImage', res.imageUrl ? res.imageUrl : null);
+        //        localStorage.setItem('userId', res.userId);
+        //    });
+        //}
+        //else {
+        //    localStorage.removeItem('currentUser');
+        //    localStorage.removeItem('auth_token');
+        //    localStorage.removeItem('currentUserId');
+        //    localStorage.removeItem('currentUserName');
+        //    localStorage.removeItem('currentUserImage');
+        //    localStorage.removeItem("userId");
+        //}
+    }
+    setLocalStorage() {
+        console.log('Loac storage');
+        if (this.username && this.username != "") {
+            localStorage.setItem('currentUser', this.username);
+            localStorage.setItem('auth_token', this.token);
             this._authService.getLoggedinObj().subscribe(res => {
                 localStorage.setItem('currentUserId', res.userId);
                 localStorage.setItem('currentUserName', res.name);
@@ -34,6 +59,11 @@ export class AppComponent {
             localStorage.removeItem('currentUserImage');
             localStorage.removeItem("userId");
         }
+        var componentObj = this;
+        setTimeout(function () {
+            console.log('Test');
+            componentObj.setLocalStorage();
+        }, 20000);
     }
     ngOnInit() {
        
