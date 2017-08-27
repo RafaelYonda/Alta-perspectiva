aa386a4e4bf6438eb1471e4bb65d3cba491b5dc5
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-08-27 11:28:37 +0600

    akash: url for nav bar fixed in forgot password

--- AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/globals.js
+++ AltaPerspectiva/src/AltaPerspectiva.Web/tsScripts/globals.js
@@ -1,6 +1,7 @@
 export var profilePath = '../../../../category/';
 export var categoryPath = '../../../../profile/';
 export var SITE_URL = 'http://alta-staging.azurewebsites.net';
+export var AUTH_SITE_URL = 'http://alta-staging-auth.azurewebsites.net';
 var AppSettings = (function () {
     function AppSettings() {
     }
@@ -8,6 +9,7 @@ var AppSettings = (function () {
 }());
 export { AppSettings };
 AppSettings.SITE_URL = 'http://alta-staging.azurewebsites.net';
+AppSettings.AUTH_SITE_URL = 'http://alta-staging-auth.azurewebsites.net';
 var LoggedInUser = (function () {
     function LoggedInUser() {
     }
