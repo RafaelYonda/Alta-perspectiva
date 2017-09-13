45acfb25d1733356d6b27529c547a4095dfaae13
DESKTOP-VBMLOV7\Dell <sharif@nascenia.com>
2017-09-13 12:50:59 +0600

    Akash: Fbsharing fixed

--- AltaPerspectiva/src/AltaPerspectiva.Web/Controllers/SocialShareController.cs
+++ AltaPerspectiva/src/AltaPerspectiva.Web/Controllers/SocialShareController.cs
@@ -15,6 +15,10 @@ namespace AltaPerspectiva.Web.Controllers
 {
     public class SocialShareController : Controller
     {
+        private string ImageUrl
+        {
+            get { return "https://altablob.blob.core.windows.net:443/category/altaperspectivafb2.png"; }
+        }
         public SocialShareController()
         {
         }
@@ -33,7 +37,7 @@ namespace AltaPerspectiva.Web.Controllers
             ViewBag.og_description = string.IsNullOrEmpty(question.Body)?"Alta Perspectiva": question.Body; 
             ViewBag.questionUrl = Startup.Url + "question/detail/"+question.Id.ToString();
             ViewBag.og_url = Startup.Url + "SocialShare/ShareQuestionInSocialMedia/" + question.Id.ToString(); ;
-            ViewBag.og_image = "https://altablob.blob.core.windows.net:443/category/altaperspectivafb.png";
+            ViewBag.og_image = ImageUrl;
             return View();
         }
         public ActionResult ShareAnswerInSocialMedia(Guid id)
@@ -68,7 +72,7 @@ namespace AltaPerspectiva.Web.Controllers
                 answer.Text = htmlDoc.DocumentNode.InnerText;
             }
             var file = new AzureFileUploadHelper();
-            var imageUrl = String.IsNullOrEmpty(answer.FirstImageUrl) ? "https://altablob.blob.core.windows.net:443/category/altaperspectivafb.png" : answer.FirstImageUrl;
+            var imageUrl = String.IsNullOrEmpty(answer.FirstImageUrl) ? ImageUrl : answer.FirstImageUrl;
             ViewBag.og_title = question.Title;
             ViewBag.og_description = answer.Text;
             ViewBag.questionUrl = Startup.Url + "question/detail/" + question.Id.ToString();
@@ -100,12 +104,12 @@ namespace AltaPerspectiva.Web.Controllers
                 blogPost.Description = htmlDoc.DocumentNode.InnerText;
             }
             var file = new AzureFileUploadHelper();
-            var imageUrl = "https://altablob.blob.core.windows.net:443/category/altaperspectivafb.png";
+            
             ViewBag.og_title = blogPost.Title;
             ViewBag.og_description = blogPost.Description;
             ViewBag.questionUrl = Startup.Url + "dashboard/blog-post/" + blogPost.BlogId.ToString();
             ViewBag.og_url = Startup.Url + "SocialShare/ShareBlog/" + blogPost.Id.ToString();
-            ViewBag.og_image = imageUrl;
+            ViewBag.og_image = ImageUrl;
 
             return View();
         }
